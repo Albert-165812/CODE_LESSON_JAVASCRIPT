a76abf61eb65c49e43e7ba797e67e0b2a498f891
albert <benjaminalbert165812@gmail.com>
2023-06-07 09:45:38 +0700

    commit third

---
+++ LESSON_1/app_lesson_1/src/pages/pageHome/pageHome.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const pageHome = () => {
+  return (
+    <div>
+        <center><h1>Đây là trang HOME</h1></center>
+    </div>
+  )
+}
+
+export default pageHome
---
+++ LESSON_1/app_lesson_1/src/pages/pageMaster.js
@@ -0,0 +1,7 @@
+import pageHome from "./pageHome/pageHome";
+import pageLogin from "./pageLogin/pageLogin";
+import pageRegister from "./pageRegister/pageRegister";
+
+export {pageHome as HOME, 
+        pageLogin as LOGIN,
+        pageRegister as REGISTER} 
